b1d46ae3a14036087c286e50187716742e083bde
dataworlddev <mohammadrezaeiut@gmail.com>
2026-06-08 17:58:08 +0330

    fix consumer to use env

--- consumer/clickhouse_client.py
+++ consumer/clickhouse_client.py
@@ -7,6 +7,6 @@ load_dotenv()
 client = get_client(
     host=os.getenv("CLICKHOUSE_HOST", "clickhouse"),
     port=int(os.getenv("CLICKHOUSE_PORT", "8123")),
-    username="admin",
-    password="admin123",
+    username=os.getenv("CLICKHOUSE_USER" , "default"),
+    password=os.getenv("CLICKHOUSE_PASSWORD", ""),
 )
